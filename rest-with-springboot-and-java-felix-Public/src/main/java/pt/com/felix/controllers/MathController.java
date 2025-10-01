8a605eb9e6715087dca7898252713887d69ba51e
Felix Eugenio Mavila <felixmacarenco@gmail.com>
2025-10-01 21:17:03 +0100

    chore: create MathControllerFile

---
+++ rest-with-springboot-and-java-felix-Public/src/main/java/pt/com/felix/controllers/MathController.java
@@ -0,0 +1,12 @@
+package pt.com.felix.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/math")
+public class MathController {
+
+
+
+}
