a2cb503cb34e20d0785086c377599c3811272643
Felix Eugenio Mavila <felixmacarenco@gmail.com>
2025-10-01 21:50:55 +0100

    feat:add verfication isNumeric on method sum

--- rest-with-springboot-and-java-felix-Public/src/main/java/pt/com/felix/controllers/MathController.java
+++ rest-with-springboot-and-java-felix-Public/src/main/java/pt/com/felix/controllers/MathController.java
@@ -1,5 +1,6 @@
 package pt.com.felix.controllers;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,6 +8,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/math")
 public class MathController {
 
+    @RequestMapping("/sum/{numberOne}/{numberTwo}")
+    public Double Sum(
+            @PathVariable("numberOne") String numberOne,
+            @PathVariable("numberTwo")   String numberTwo
+    ) throws Exception {
+
+        if(!isNumeric(numberOne) || !isNumeric(numberTwo)) throw new IllegalArgumentException();
+        return  1D;
+    }
+
+    private boolean isNumeric(String numberOne){
+     return false;
+
+    }
 
 
 }
