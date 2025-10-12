ea0e962bd90a3823ec6def0cd4c19674ef14f9e9
Felix Eugenio Mavila <felixmacarenco@gmail.com>
2025-10-12 16:15:14 +0100

    feat: add PersonDTO file

---
+++ rest-with-springboot-and-java-felix-Public/src/main/java/pt/com/felix/data/dto/PersonDTO.java
@@ -0,0 +1,78 @@
+package pt.com.felix.data.dto;
+import jakarta.persistence.*;
+import java.io.Serializable;
+import java.util.Objects;
+
+public class PersonDTO implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private long id;
+    private String firstName;
+    private String lastName;
+    private int age;
+    private String gender;
+    private String address;
+
+    public PersonDTO() {}
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        PersonDTO person = (PersonDTO) o;
+        return id == person.id && age == person.age && Objects.equals(firstName, person.firstName) && Objects.equals(lastName, person.lastName) && Objects.equals(gender, person.gender);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, firstName, lastName, age, gender);
+    }
+}
