64f448b98b7929a9ad3cdb24963ac79ba390e2d0
Felix Eugenio Mavila <felixmacarenco@gmail.com>
2025-10-12 10:35:37 +0100

    feat: add findAll method in PersonService

--- rest-with-springboot-and-java-felix-Public/src/main/java/pt/com/felix/services/PersonService.java
+++ rest-with-springboot-and-java-felix-Public/src/main/java/pt/com/felix/services/PersonService.java
@@ -3,6 +3,8 @@ package pt.com.felix.services;
 import org.springframework.stereotype.Service;
 import pt.com.felix.models.Person;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Logger;
 
@@ -12,6 +14,14 @@ public class PersonService {
     private AtomicLong counter = new AtomicLong();
     private Logger logger = Logger.getLogger(PersonService.class.getName());
 
+    public Person findByAll(){
+        List<Person> persons = new ArrayList<Person>();
+        for(int i = 0; i < 10; i++){
+            Person person = mockPerson(i);
+            persons.add(person);
+        }
+        return (Person) persons;
+    }
 
     public Person findById(String id){
      logger.info("Finding person with id: " + id);
@@ -23,5 +33,14 @@ public class PersonService {
      person.setAge(25);
      person.setGender("Male");
      return person;
+    }
+
+    private Person mockPerson(int i){
+        Person person = new Person();
+        person.setId(counter.incrementAndGet());
+        person.setFirstName("Felix");
+        person.setLastName("Felipe");
+        person.setAge(25);
+        return person;
     }
 }
